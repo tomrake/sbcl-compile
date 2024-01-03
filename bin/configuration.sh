4b0a17d1f45025a6950db7cfa36f6d5562912e74
Tom Rake <zzzap1957@gmail.com>
2024-01-03 18:06:14 -0500

    Add sh/configure.sh and source it from sbcl-compile.sh.

---
+++ bin/configuration.sh
@@ -0,0 +1,29 @@
+
+export BOOTING_COMPILER="${PWD}/booting/sbcl/sbcl.exe"
+export BOOTING_COMPILER_OPTIONS="--no-userinit --no-sysinit" 
+#### NOTE
+## This is msys2/ucrt64 specific
+##
+### See sbcl source:  INSTALL 2.3 Troubleshooting "GNU Make not found"
+export COMPILE_BUILD_OPTIONS="--fancy"
+
+export GNUMAKE="c:/devel/msys64/ucrt64/bin/mingw32-make.exe"
+## The release version we reference
+export RELEASE_VERSION="2.4.0"
+## The public signature of the signer   
+export SIGNING_KEY=D6839CA0A67F74D9DFB70922EBD595A9100D63CD
+## The current signer
+export SIGNER=crhodes
+## Where the GPG keys are stored
+export KEY_BASE=./.gnupg
+## THe signed files
+export SIGNING_FILE=${SIGNER}.asc
+
+## This is where the directory where the project is to be built in.
+export BUILD_LOCATION="${PWD}/build-fancy"
+
+export BUILD_LOG=build-log.txt
+
+export TEST_LOG=test-log.txt
+
+export ANSI_TEST_LOG=ansi-test-log.txt
